d51627fbe6cb6b78951a20abb975496177c8a8ae
root <root@DESKTOP-UEO62DE.localdomain>
2023-12-01 16:50:31 +0300

    Complete count_sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
+  def __init__(self, value = None):
+    self._value = None
+    self.value = value
+
+  @property
+
+  def value (self):
+    return self._value
+  
+  @value.setter
+
+  def value (self, new_value):
+    if isinstance(new_value, str):
+      self._value = new_value
+
+  def is_sentence (self):
+    if self._value is not None:
+      return self._value.endswith('.')
+    return False
+  
+  def is_question(self):
+    if self._value is not None:
+      return self._value.endswith('?')
+    return False
+  
+  def is_exclamation(self):
+    if self._value is not None:
+      return self._value.endswith("?")
+    return False
+  
+  def count_sentences (self):
+    if self._value is not None:
+      sentences = [sentences.strip() for sentence in re.split(r'[.!?]', self._value) if sentence]
+      return len(sentences)
+    return 0
+
   pass
